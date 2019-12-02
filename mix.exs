ce4e0d0165f8ce189bbc94f792c45ba4cb5e66ec
Mathijs Saey <mathsaey@vub.ac.be>
2019-12-02 13:12:36 +0100

    Remove automatic reloading

--- mix.exs
+++ mix.exs
@@ -6,14 +6,7 @@ defmodule Aoc.MixProject do
       app: :aoc,
       version: "0.1.0",
       elixir: "~> 1.7",
-      deps: deps()
-    ]
-  end
-
-  defp deps do
-    [
-      # Automatically reload code on change
-      {:remix, "~> 0.0.1", only: :dev},
+      deps: []
     ]
   end
 end
