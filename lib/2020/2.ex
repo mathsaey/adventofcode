161770e2580149a47456c81c42a9c38baa15203b
Mathijs Saey <mathijs@mathsaey.be>
2020-12-02 11:26:47 +0100

    2020 day 2

---
+++ lib/2020/2.ex
@@ -0,0 +1,40 @@
+import AOC
+
+aoc 2020, 2 do
+  def example_input do
+    """
+      1-3 a: abcde
+      1-3 b: cdefg
+      2-9 c: ccccccccc
+    """
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&String.trim/1)
+  end
+
+  def p1, do: solve(&verify_p1/1)
+  def p2, do: solve(&verify_p2/1)
+
+  defp solve(verify) do
+    input_stream()
+    |> Stream.map(&parse/1)
+    |> Stream.filter(verify)
+    |> Enum.count()
+  end
+
+  defp parse(str) do
+    [_, x, y, c, str] = Regex.run(~r/(\d+)-(\d+) (.)+: (.+)/, str)
+    {String.to_integer(x), String.to_integer(y), c, str}
+  end
+
+  defp verify_p1({min, max, c, str}) do
+    count = str |> String.codepoints() |> Enum.count(&(&1 == c))
+    min <= count and count <= max
+  end
+
+  defp verify_p2({p1, p2, c, str}) do
+    c1 = String.at(str, p1 - 1) == c
+    c2 = String.at(str, p2 - 1) == c
+    (c1 or c2) and not (c1 and c2)
+  end
+end
