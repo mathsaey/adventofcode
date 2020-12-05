269db413f2bc308e38158a255f625fe99c934569
Mathijs Saey <mathijs@mathsaey.be>
2020-12-05 23:04:34 +0100

    day 5

---
+++ lib/2020/5.ex
@@ -0,0 +1,29 @@
+import AOC
+
+aoc 2020, 5 do
+  def p1, do: input_stream() |> Stream.map(&id/1) |> Enum.max()
+
+  def p2 do
+    ids = input_stream() |> Stream.map(&id/1) |> MapSet.new()
+
+    0..(127 * 8 + 8)
+    |> Enum.filter(&(&1 not in ids and (&1 - 1) in ids and (&1 + 1) in ids))
+    |> hd()
+  end
+
+  def id(<<row::binary-size(7), col::binary-size(3)>>) do
+    row = decode(row, 0..127, "F", "B")
+    col = decode(col, 0..7, "L", "R")
+    row * 8 + col
+  end
+
+  def decode(str, range, lower, upper) do
+    str
+    |> String.graphemes()
+    |> Enum.reduce(range, fn
+      ^lower, range -> %{range | last: range.last - round(Enum.count(range) / 2)}
+      ^upper, range -> %{range | first: range.first + round(Enum.count(range) / 2)}
+    end)
+    |> Enum.at(0)
+  end
+end
