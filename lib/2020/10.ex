344020e02d6c6987467c97456b40d80d4534fd32
Mathijs Saey <mathijs@mathsaey.be>
2020-12-10 13:13:45 +0100

    Day 10

---
+++ lib/2020/10.ex
@@ -0,0 +1,31 @@
+import AOC
+
+aoc 2020, 10 do
+  def p1 do
+    %{1 => f_1, 3 => f_3} = diffs() |> Enum.concat([3]) |> Enum.frequencies()
+    f_1 * f_3
+  end
+
+  def p2, do: count(diffs()) + 1
+
+  def input_stream, do: super() |> Stream.map(&String.to_integer/1)
+  def diffs, do: input_stream() |> Enum.sort() |> Enum.map_reduce(0, &{&1 - &2, &1}) |> elem(0)
+
+  # Start from the list of differences of the longest possible combination. A new combination is
+  # possible if you can "collapse" two differences into one. Recursively count all possible
+  # variants of the original list and the modified one.
+  # A lot of work is done multiple times, so we memoise to offset this
+  def count([l, r | tl]) when l + r <= 3, do: 1 + memoised([l + r | tl]) + memoised([r | tl])
+  def count([_ | tl]), do: memoised(tl)
+  def count([]), do: 0
+
+  def memoised(args) do
+    if res = Process.get(args) do
+      res
+    else
+      res = count(args)
+      Process.put(args, res)
+      res
+    end
+  end
+end
