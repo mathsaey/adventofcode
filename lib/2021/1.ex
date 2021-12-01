08d17e588a632fafe392730dd09b9bb906be0428
Mathijs Saey <mathijs@mathsaey.be>
2021-12-01 10:08:36 +0100

    day 1

---
+++ lib/2021/1.ex
@@ -0,0 +1,23 @@
+import AOC
+
+aoc 2021, 1 do
+  def input_stream, do: super() |> Stream.map(&String.to_integer/1)
+  def p1, do: input_stream() |> count_increasing()
+
+  def p2 do
+    input_stream()
+    |> Stream.transform([], fn
+      el, [p1, p2] -> {[el + p1 + p2], [el, p1]}
+      el, lst -> {[], [el | lst]}
+    end)
+    |> count_increasing()
+  end
+
+  def count_increasing(enum) do
+    enum
+    |> Enum.reduce({0, -1}, fn el, {prev, ctr} ->
+      if el > prev, do: {el, ctr + 1}, else: {el, ctr}
+    end)
+    |> elem(1)
+  end
+end
