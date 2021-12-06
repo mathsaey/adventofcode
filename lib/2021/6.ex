30764d057548fb46ffd95b46d22b5cbfd072998a
Mathijs Saey <mathijs@mathsaey.be>
2021-12-06 08:14:10 +0100

    day 6

---
+++ lib/2021/6.ex
@@ -0,0 +1,32 @@
+import AOC
+
+aoc 2021, 6 do
+  def p1, do: solve(80)
+  def p2, do: solve(256)
+
+  def solve(days), do: input() |> to_map() |> simulate_loop(days) |> count()
+
+  def input do
+    input_string() |> String.trim() |> String.split(",") |> Enum.map(&String.to_integer/1)
+  end
+
+  def to_map(lst) do
+    Enum.reduce(lst, %{}, fn timer, map -> Map.update(map, timer, 1, &(&1 + 1)) end)
+  end
+
+  def simulate({0, amount}), do: [{6, amount}, {8, amount}]
+  def simulate({timer, amount}), do: [{timer - 1, amount}]
+
+  def simulate_step(state) do
+    state
+    |> Enum.flat_map(&simulate/1)
+    |> Enum.reduce(%{}, fn
+      {timer, amount}, map -> Map.update(map, timer, amount, &(&1 + amount))
+    end)
+  end
+
+  def simulate_loop(state, 0), do: state
+  def simulate_loop(state, n), do: state |> simulate_step() |> simulate_loop(n - 1)
+
+  def count(state), do: state |> Map.values() |> Enum.sum()
+end
