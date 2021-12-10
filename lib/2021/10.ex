a5bd0156c0beb02a85864790f040cd1a77564767
Mathijs Saey <mathijs@mathsaey.be>
2021-12-10 10:07:32 +0100

    Day 10

---
+++ lib/2021/10.ex
@@ -0,0 +1,50 @@
+import AOC
+
+aoc 2021, 10 do
+  def p1 do
+    input_stream()
+    |> Stream.map(&parse/1)
+    |> Stream.filter(&corrupted?/1)
+    |> Stream.map(&hd/1)
+    |> Stream.map(&elem(&1, 1))
+    |> Stream.map(&syntax_score/1)
+    |> Enum.sum()
+  end
+
+  def p2 do
+    scores =
+      input_stream()
+      |> Stream.map(&parse/1)
+      |> Stream.reject(&corrupted?/1)
+      |> Stream.map(fn [{:remaining, lst}] -> lst end)
+      |> Stream.map(fn lst ->
+        lst |> Enum.map(&completion_score/1) |> Enum.reduce(0, &(&2 * 5 + &1))
+      end)
+      |> Enum.sort()
+
+    Enum.drop(scores, div(length(scores), 2)) |> hd()
+  end
+
+  def corrupted?([{:delimiter, _} | _]), do: true
+  def corrupted?(_), do: false
+
+  def syntax_score(?)), do: 3
+  def syntax_score(?]), do: 57
+  def syntax_score(?}), do: 1197
+  def syntax_score(?>), do: 25137
+
+  def completion_score(?(), do: 1
+  def completion_score(?[), do: 2
+  def completion_score(?{), do: 3
+  def completion_score(?<), do: 4
+
+  def parse(str), do: parse(str, [])
+  def parse(<<>>, []), do: []
+  def parse(<<>>, stack), do: [{:remaining, stack}]
+  def parse(<<c, r::binary>>, stack) when c in [?(, ?[, ?{, ?<], do: parse(r, [c | stack])
+  def parse(<<?), r::binary>>, [?( | stack]), do: parse(r, stack)
+  def parse(<<?], r::binary>>, [?[ | stack]), do: parse(r, stack)
+  def parse(<<?}, r::binary>>, [?{ | stack]), do: parse(r, stack)
+  def parse(<<?>, r::binary>>, [?< | stack]), do: parse(r, stack)
+  def parse(<<c, r::binary>>, [_ | stack]), do: [{:delimiter, c} | parse(r, stack)]
+end
