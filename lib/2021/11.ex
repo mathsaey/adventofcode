4588904ab7b6a1d4b82b05f328c547eeb255c15e
Mathijs Saey <mathijs@mathsaey.be>
2021-12-11 12:41:56 +0100

    Day 11

---
+++ lib/2021/11.ex
@@ -0,0 +1,58 @@
+import AOC
+
+aoc 2021, 11 do
+  def p1 do
+    input_stream()
+    |> parse()
+    |> Stream.iterate(&step/1)
+    |> Stream.map(&Enum.count(&1, fn {_, v} -> v == 0 end))
+    |> Stream.take(101)
+    |> Enum.sum()
+  end
+
+  def p2 do
+    input_stream()
+    |> parse()
+    |> Stream.iterate(&step/1)
+    |> Stream.map(&Enum.all?(&1, fn {_, v} -> v == 0 end))
+    |> Stream.take_while(&not/1)
+    |> Enum.to_list()
+    |> length()
+  end
+
+  def parse(stream) do
+    stream
+    |> Stream.map(&String.to_integer/1)
+    |> Stream.map(&Integer.digits/1)
+    |> Stream.with_index()
+    |> Stream.flat_map(fn {lst, y} ->
+      lst |> Enum.with_index() |> Enum.map(fn {el, x} -> {{x, y}, el} end)
+    end)
+    |> Map.new()
+  end
+
+  def step(map), do: map |> increase_all() |> flash() |> reset()
+
+  def increase_all(map), do: map |> Enum.map(fn {c, v} -> {c, v + 1} end) |> Map.new()
+
+  def flash(map) do
+    keys = map |> Enum.filter(fn {_, v} -> v != :flash and v > 9 end) |> Enum.map(&elem(&1, 0))
+    map = Enum.reduce(keys, map, &flash/2)
+    if keys == [], do: map, else: flash(map)
+  end
+
+  def flash(coord, map) do
+    map = Map.put(map, coord, :flash)
+    coord |> adjacent() |> Enum.reduce(map, &increase/2)
+  end
+
+  def adjacent({x, y}), do: for x <- (x - 1)..(x + 1), y <- (y - 1)..(y + 1), do: {x, y}
+
+  def increase(coord, map) when is_map_key(map, coord), do: %{map | coord => inc(map[coord])}
+  def increase(_, map), do: map
+
+  def inc(:flash), do: :flash
+  def inc(x), do: x + 1
+
+  def reset(map), do: map |> Enum.map(fn {c, v} -> {c, if(v > 9, do: 0, else: v)} end) |> Map.new()
+end
