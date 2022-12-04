dd94553c17256effaa32ba45649e5791ac75372e
Mathijs Saey <mathijs@mathsaey.be>
2022-12-04 16:54:25 +0100

    Improve with Range.disjoint/2

--- lib/2022/4.ex
+++ lib/2022/4.ex
@@ -1,16 +1,14 @@
 import AOC
 
 aoc 2022, 4 do
-  def p1(input), do: solve(input, fn {l, r} -> superset?(l, r) end)
-  def p2(input), do: solve(input, fn {l, r} -> overlap?(l, r) end)
+  def p1(input), do: solve(input, fn {l, r} -> contained?(l, r) or contained?(r, l) end)
+  def p2(input), do: solve(input, fn {l, r} -> not Range.disjoint?(l, r) end)
 
   def solve(input, filter) do
     input |> String.split("\n") |> Enum.map(&parse_line/1) |> Enum.filter(filter) |> Enum.count()
   end
 
-  def superset?(l, r), do: contained?(l, r) or contained?(r, l)
   def contained?(f1..t1, f2..t2), do: f1 >= f2 and t1 <= t2
-  def overlap?(f..t, r), do: (f in r) or (t in r) or superset?(f..t, r)
 
   def parse_line(str) do
     ~r/(\d+)-(\d+),(\d+)-(\d+)/
