7084a657b4531eabbfbb44d26206fa92be716382
Mathijs Saey <mathijs@mathsaey.be>
2022-12-06 22:37:29 +0100

    bump aoc_utils

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule Aoc.MixProject do
 
   defp deps do
     [
-      {:advent_of_code_utils, "~> 3.0"},
+      {:advent_of_code_utils, "~> 3.1"},
       {:heap, "~> 2.0"},
       {:qex, "~> 0.5"}
     ]
