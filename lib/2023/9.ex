e11861ebf3a2988e525640e4c4e1d5066bcec8c9
Mathijs Saey <mathijs@mathsaey.be>
2023-12-09 16:51:20 +0100

    day 9

---
+++ lib/2023/9.ex
@@ -0,0 +1,26 @@
+import AOC
+
+aoc 2023, 9 do
+  def p1(input), do: input |> parse() |> solve()
+  def p2(input), do: input |> parse() |> Enum.map(&Enum.reverse/1) |> solve()
+  def solve(lsts), do: lsts |> Enum.map(&get_next/1) |> Enum.sum()
+
+  def get_next(lst) do
+    lst
+    |> Stream.iterate(&difference/1)
+    |> Enum.take_while(fn lst -> Enum.any?(lst, &(&1 != 0)) end)
+    |> Enum.map(&List.last/1)
+    |> Enum.reverse()
+    |> Enum.reduce(0, &(&1 + &2))
+  end
+
+  def difference(lst) do
+    lst |> Enum.chunk_every(2,1, :discard) |> Enum.map(fn [cur, next] -> next - cur end)
+  end
+
+  def parse(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(fn line -> line |> String.split() |> Enum.map(&String.to_integer/1) end)
+  end
+end
