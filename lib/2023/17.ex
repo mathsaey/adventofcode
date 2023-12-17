7321a3939e25048ae105360cc74b83e8c6e499f6
Mathijs Saey <mathijs@mathsaey.be>
2023-12-17 20:47:50 +0100

    day 17

---
+++ lib/2023/17.ex
@@ -0,0 +1,59 @@
+import AOC
+
+aoc 2023, 17 do
+  def p1(input), do: input |> parse() |> find_path(&p1_line/2)
+  def p2(input), do: input |> parse() |> find_path(&p2_line/2)
+
+  def find_path(grid = {_, {_..max_x, _..max_y}}, line_fun) do
+    [{0, 0, :v}, {0, 0, :h}]
+    |> Enum.map(&{manhattan(&1, {max_x, max_y}), &1})
+    |> Enum.reduce({Heap.min(), %{}}, fn el = {_, option}, {queue, cheapest_paths} ->
+      {Heap.push(queue, el), Map.put(cheapest_paths, option, 0)}
+    end)
+    |> a_star(line_fun, {max_x, max_y}, grid)
+  end
+
+  def a_star({queue, cheapest_paths}, line_fun, goal, {grid, bounds}) do
+    {{_, current = {x, y, _}}, queue} = Heap.split(queue)
+    current_score = cheapest_paths[current]
+
+    if {x, y} == goal do
+      current_score
+    else
+      current
+      |> options(bounds, line_fun)
+      |> Enum.reduce({queue, cheapest_paths}, fn option, {queue, cheapest_paths} ->
+        score = current_score + cost(current, option, grid)
+        if not Map.has_key?(cheapest_paths, option) or score < cheapest_paths[option] do
+          heuristic = score + manhattan(option, goal)
+          {Heap.push(queue, {heuristic, option}), Map.put(cheapest_paths, option, score)}
+        else
+          {queue, cheapest_paths}
+        end
+      end)
+      |> a_star(line_fun, goal, {grid, bounds})
+    end
+  end
+
+  def options({x, y, :v}, {bounds, _}, line), do: x |> line.(bounds) |> Enum.map(&{&1, y, :h})
+  def options({x, y, :h}, {_, bounds}, line), do: y |> line.(bounds) |> Enum.map(&{x, &1, :v})
+
+  def p1_line(i, min..max), do: for(j <- i - 3..i + 3, j != i, j >= min, j <= max, do: j)
+
+  def p2_line(i, min..max) do
+    Enum.concat(
+      for(j <- i + 4..i + 10, j <= max, do: j),
+      for(j <- i - 10..i - 4, j >= min, do: j)
+    )
+  end
+
+  def cost({x_from, y_from, _}, {x_to, y_to, _}, grid) do
+    for x <- x_from..x_to, y <- y_from..y_to, not (x == x_from and y == y_from), reduce: 0 do
+      sum -> sum + grid[{x, y}]
+    end
+  end
+
+  def manhattan({x, y, _}, {goal_x, goal_y}), do: abs(goal_x - x) + abs(goal_y - y)
+
+  def parse(input), do: input |> Utils.Grid.input_to_map_with_bounds(&String.to_integer/1)
+end
--- lib/utils.ex
+++ lib/utils.ex
@@ -14,9 +14,34 @@ defmodule Utils do
     @moduledoc """
     Functions to work with grids.
 
-    The grids should be represented as maps.
+    The grids are represented as maps.
     """
 
     # TODO: neighbours, two and three dimensional. Option to include diagonals or not.
+
+    def bounds(grid) do
+      coords = Map.keys(grid)
+      {min_x, max_x} = coords |> Enum.map(fn {x, _} -> x end) |> Enum.min_max()
+      {min_y, max_y} = coords |> Enum.map(fn {_, y} -> y end) |> Enum.min_max()
+      {min_x..max_x, min_y..max_y}
+    end
+
+    def input_to_map(input, parse_el \\ &Function.identity/1) do
+      input
+      |> String.split("\n")
+      |> Enum.with_index()
+      |> Enum.flat_map(fn {line, y} ->
+        line
+        |> String.graphemes()
+        |> Enum.with_index()
+        |> Enum.map(fn {el, x} -> {{x, y}, parse_el.(el)} end)
+      end)
+      |> Map.new()
+    end
+
+    def input_to_map_with_bounds(input, parse_el \\ &Function.identity/1) do
+      grid = input_to_map(input, parse_el)
+      {grid, bounds(grid)}
+    end
   end
 end
